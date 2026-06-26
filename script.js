60b9937cb48a7c365553d6d4d7817b307d5386ce
SFTmanager <sft.management.javs@gmail.com>
2026-06-26 22:31:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ tg.ready();
 // Пример получения данных пользователя
 const user = tg.initDataUnsafe.user;
 if (user) {
-  document.getElementById('user-name').innerHTML = '${user.first_name}';
-  document.getElementById('user-tg-id').innerHTML = '${user.id}';
+  document.getElementById('user-name').textContent = '${user.first_name}';
+  document.getElementById('user-tg-id').textContent = '${user.id}';
 }
