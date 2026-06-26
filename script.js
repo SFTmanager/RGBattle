c19fca2fe45cb57918aaa8e5b8042db2a1884e38
SFTmanager <sft.management.javs@gmail.com>
2026-06-26 22:36:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-// Инициализация Web App
-const tg = window.Telegram.WebApp;
-tg.ready();
-// Пример получения данных пользователя
-const user = tg.initDataUnsafe.user;
-if (user) {
-  document.getElementById('user-name').textContent = '${user.first_name}';
-  document.getElementById('user-tg-id').textContent = '${user.id}';
-}
+// Ждем, пока весь HTML загрузится
+document.addEventListener('DOMContentLoaded', () => {
+    const tg = window.Telegram.WebApp;
+    tg.ready();
+
+    const user = tg.initDataUnsafe.user;
+    
+    if (user) {
+        // Используем ` (обратные кавычки) вместо ' (одинарных)
+        document.getElementById('user-name').textContent = `${user.first_name}`;
+        document.getElementById('user-tg-id').textContent = `${user.id}`;
+    } else {
+        document.getElementById('user-name').textContent = "Гость";
+    }
+});
